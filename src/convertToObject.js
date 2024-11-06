4f11460aaeec150da9549ddf24c82d779a956b48
Your Name <vitaliy.savenets@gmail.com>
2024-11-06 21:27:23 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let res = sourceString.split(';');
+
+  res = res.map((el) => el.split(':'));
+  res = res.map((el) => el.map((item) => item.trim()));
+  res = res.filter((item) => item.length > 1);
+
+  res = res.reduce((acc, item) => {
+    return {
+      ...acc,
+      [item[0]]: item[1],
+    };
+  }, {});
+
+  return res;
 }
 
 module.exports = convertToObject;
